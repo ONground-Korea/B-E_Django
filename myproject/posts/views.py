5ee962d236bb45976a99666335c76a62695a63df
ONground-Korea <onground@korea.ac.kr>
2021-04-05 23:48:51 +0900

    과제1

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("aaaaa")
+    if request.method == "GET":
+       return JsonResponse({'req_method':'get'})
+
+    elif request.method == "POST":
+        return JsonResponse({'req_method':'post'})
+
+    else:
+        return JsonResponse({'error': 'request method not allowed'}, status = 405)
